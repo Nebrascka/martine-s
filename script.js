bad06ac1881067ac46994d66a72fcc148896037c
David Owino <71890256+Nebrascka@users.noreply.github.com>
2025-01-29 22:29:55 +0300

    menu buttons

--- script.js
+++ script.js
@@ -68,3 +68,14 @@ document.addEventListener("DOMContentLoaded", () => {
       })
       .catch(error => console.error("Error loading the navbar:", error));
   });
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".nav-link");
+
+    hamburger.addEventListener("click", function () {
+        navLinks.classList.toggle("show");
+        hamburger.classList.toggle("active");
+    });
+});
